9f56e65df6ac642573dea4e1401207e6eb465cfd
Михаил <spaik.mironov@yandex.ru>
2021-05-10 17:40:06 +0700

    api_2

--- shop/mainapp/api/api_views.py
+++ shop/mainapp/api/api_views.py
@@ -1,10 +1,28 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
 
-from .serializer import CategorySerializer
-from ..models import Category
+from .serializer import (
+    CategorySerializer,
+    SmartphoneSerializer,
+    NotebookSerializer,
+)
+from ..models import Category, Smartphone, Notebook
 
 
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.object.all()
 
+
+class SmartphoneListAPIView(ListAPIView):
+    serializer_class = SmartphoneSerializer
+    queryset = Smartphone.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['price', 'title', 'accum_volume']
+
+
+class NotebookListAPIView(ListAPIView):
+    serializer_class = NotebookSerializer
+    queryset = Notebook.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['price', 'title', 'video']
