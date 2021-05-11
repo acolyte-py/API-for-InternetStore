c46d28b352ce04882d2cbd8a3b8eecc6617574d5
Михаил <spaik.mironov@yandex.ru>
2021-05-11 11:13:23 +0700

    api_4

--- shop/mainapp/api/api_views.py
+++ shop/mainapp/api/api_views.py
@@ -1,5 +1,9 @@
+from collections import OrderedDict
+
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 
 from .serializer import (
     CategorySerializer,
@@ -10,13 +14,29 @@ from .serializer import (
 from ..models import Category, Smartphone, Notebook, Customer
 
 
+class Pagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count_objects', self.page.paginator.count),
+            ('next_item', self.get_next_link()),
+            ('previous_item', self.get_previous_link()),
+            ('items', data)
+        ]))
+
+
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializer
+    pagination_class = Pagination
     queryset = Category.object.all()
 
 
 class SmartphoneListAPIView(ListAPIView):
     serializer_class = SmartphoneSerializer
+    pagination_class = Pagination
     queryset = Smartphone.objects.all()
     filter_backends = [SearchFilter]
     search_fields = ['price', 'title', 'accum_volume']
@@ -24,6 +44,7 @@ class SmartphoneListAPIView(ListAPIView):
 
 class NotebookListAPIView(ListAPIView):
     serializer_class = NotebookSerializer
+    pagination_class = Pagination
     queryset = Notebook.objects.all()
     filter_backends = [SearchFilter]
     search_fields = ['price', 'title', 'video']
@@ -31,12 +52,14 @@ class NotebookListAPIView(ListAPIView):
 
 class SmartphoneDetailAPIView(RetrieveAPIView):
     serializer_class = SmartphoneSerializer
+    pagination_class = Pagination
     queryset = Smartphone.objects.all()
     lookup_field = 'id'
 
 
 class NotebookDetailAPIView(RetrieveAPIView):
     serializer_class = NotebookSerializer
+    pagination_class = Pagination
     queryset = Notebook.objects.all()
     lookup_field = 'id'
 
@@ -44,5 +67,6 @@ class NotebookDetailAPIView(RetrieveAPIView):
 class CustomersListAPIView(ListAPIView):
     """Это для практики, в реале такой функционал делать не стоит"""
     serializer_class = CustomerSerializer
+    pagination_class = Pagination
     queryset = Customer.objects.all()
 
